e263c0df8c61fab003123a2ad62525e36e421fb9
Tristan Lapointe <tristan.lapointe02@gmail.com>
2021-02-26 13:05:58 -0500

    Ajout du dossier data. Affichage des dossiers.

--- src/Dossier.jsx
+++ src/Dossier.jsx
@@ -3,9 +3,9 @@ import "./Dossier.scss"
 export default function Dossier(props) {
     return(
         <li>
-            <img src={"images-dossiers/" + props.id + ".png"} alt={props.nom}/>
+            <img src={"images-dossiers/" + props.id + ".png"} alt={props.titre}/>
             <div className="info">
-                <h3 className="nom">{props.nom}</h3>
+                <h3 className="titre">{props.titre}</h3>
                 <p className="date">{props.date}</p>
             </div>
         </li>
--- src/ListeDossiers.jsx
+++ src/ListeDossiers.jsx
@@ -1,11 +1,15 @@
 import Dossier from "./Dossier"
 import "./ListeDossiers.scss"
+import tabDossiers from "./data/dossiers.json"
 
 export default function ListeDossiers(props) {
     return (
         <div className="ListeDossiers">
             <ul className="ul-dossiers">
-                <Dossier />
+            {
+                //Utilisation d'une fonction fléchée
+                tabDossiers.map(unDoss => <Dossier key={unDoss.id} titre={unDoss.titre} couleur={unDoss.couleur} date={unDoss.date} id={unDoss.id}/>)
+            }
             </ul>
         </div>
     )
